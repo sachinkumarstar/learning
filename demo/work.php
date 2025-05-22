e184ccb0931491703bed120d09c62f26816fad83
sachinkumarstar <sachinkumarstar007@gmail.com>
2025-05-22 16:31:42 +0530

    new changes

--- demo/work.php
+++ demo/work.php
@@ -1,3 +1,20 @@
 <?php
 echo "Work file New";
+
+
+echo "Sachin Kumar";
+$subjects = [
+    ['Subject Code' => 'MATH101', 'Subject Name' => 'Mathematics'],
+    ['Subject Code' => 'ENG102', 'Subject Name' => 'English Literature'],
+    ['Subject Code' => 'SCI103', 'Subject Name' => 'General Science'],
+    ['Subject Code' => 'HIST104', 'Subject Name' => 'World History']
+];
+
+echo "<table border='1'>";
+echo "<tr><th>Subject Code</th><th>Subject Name</th></tr>";
+foreach ($subjects as $subject) {
+    echo "<tr><td>{$subject['Subject Code']}</td><td>{$subject['Subject Name']}</td></tr>";
+}
+echo "</table>";
+
 ?>
